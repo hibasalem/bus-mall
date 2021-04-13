0219a12889c6949fa658d30c67f18dfbf50ebedc
hibasalem <salemhiba.hs@gmail.com>
2021-04-13 13:38:33 +0300

    fixed lab11

--- js/app.js
+++ js/app.js
@@ -6,21 +6,16 @@ let rightElement = document.getElementById('right');
 
 let maxAteempts = 25;
 let userAteempts = 0;
-
-
 let leftIndex = 0;
 let centerIndex = 0;
 let rightIndex = 0;
-
 let photos = [];
 
 function Photo(name, source) {
     this.name = name;
     this.source = source;
-
     this.showsCounter = 0;
     this.votes = 0;
-
     photos.push(this);
 }
 
@@ -28,7 +23,7 @@ new Photo('bag', 'img/bag.jpg');
 new Photo('banana', 'img/banana.jpg');
 new Photo('bathroom', 'img/bathroom.jpg');
 new Photo('boots', 'img/boots.jpg');
-new Photo('bathroom', 'img/bathroom.jpg');
+new Photo('breakfast', 'img/breakfast.jpg');
 new Photo('bubblegum', 'img/bubblegum.jpg');
 new Photo('chair', 'img/chair.jpg');
 new Photo('cthulhu', 'img/cthulhu.jpg');
@@ -45,31 +40,20 @@ new Photo('usb', 'img/usb.gif');
 new Photo('water-can', 'img/water-can.jpg');
 new Photo('wine-glass', 'img/wine-glass.jpg');
 
-console.log(photos);
-
 function RandIndex() {
-
     return Math.floor(Math.random() * photos.length);
 }
-
 // showing  on the photos 
-
 function renderImg() {
 
     leftIndex = RandIndex();
     centerIndex = RandIndex();
     rightIndex = RandIndex();
+
     while (leftIndex === centerIndex || leftIndex === rightIndex || centerIndex === rightIndex) {
 
-        while (leftIndex === rightIndex) {
-            rightIndex = RandIndex();
-        }
-        while (leftIndex === centerIndex) {
-            centerIndex = RandIndex();
-        }
-        while (centerIndex === rightIndex) {
-            rightIndex = RandIndex();
-        }
+        rightIndex = RandIndex();
+        centerIndex = RandIndex();
     }
 
     leftElement.src = photos[leftIndex].source;
@@ -97,45 +81,39 @@ function handleUserClick(event) {
             photos[leftIndex].votes++;
         } else if (event.target.id === 'center') {
             photos[centerIndex].votes++;
-        } else {
+        } else if (event.target.id === 'right') {
             photos[rightIndex].votes++;
+        } else {
+            userAteempts--;
         }
         renderImg();
 
     } else {
         //create the button 
+        imgContanair.removeEventListener('click', handleUserClick);
         let result = document.getElementById('button');
         let button = document.createElement('button');
         result.appendChild(button);
-        button.textContent ="show results"
+        button.textContent = "show results"
 
         //create the list when click on button 
 
         button.addEventListener('click', makelist);
-
         function makelist() {
-
             let list = document.getElementById('results');
             let listli = 0;
-
             for (let i = 0; i < photos.length; i++) {
                 listli = document.createElement('li');
                 list.appendChild(listli);
                 listli.textContent = `${photos[i].name} has ${photos[i].votes} votes ,and was seen ${photos[i].showsCounter} times`
             }
-
-            imgContanair.removeEventListener('click', handleUserClick);
         }
-
         //hide the button
-
         button.addEventListener('click', hideIt);
         function hideIt() {
 
-            document.getElementById('button').style.display = 'block'; 
-            this.style.display = 'none';            
+            document.getElementById('button').style.display = 'block';
+            this.style.display = 'none';
         }
     }
 }
-
-
